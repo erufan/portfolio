c99958c9340875d52b4414c20c4a9d85ea03a625
erfan taheri <erfanta197@gmail.com>
2024-09-15 00:37:28 +0330

    render cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import data from "./data/data.json";
+import Card from "./components/Card";
+import CardsLayouts from "./layouts/CardsLayouts";
+
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <CardsLayouts>
+      {data.projects.map((p) => (
+        <Card
+          key={p.title}
+          title={p.title}
+          image={`public/${p?.picturs[0]}`}
+          description={p.description}
+          code={p.code}
+          link={p.link}
+        />
+      ))}
+    </CardsLayouts>
+  );
 };
 
 export default App;
