07d3c6f689c07418a106ce69f0be5d1ea027606c
erfan taheri <erfanta197@gmail.com>
2024-09-15 23:33:45 +0330

    update style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,17 +13,21 @@ interface Props {
 const Card = ({ title, images, description, code, link }: Props) => {
   return (
     <div className={style.gridItem}>
-      <Slider images={images} />
-      <h2 className={style.title}>{title}</h2>
-      <p>
-        <ExpandedText children={description} />
-      </p>
-      <a href={code} target="_blank" rel="noopener noreferrer">
-        نمایش کد
-      </a>
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        مشاهده پروژه
-      </a>
+      <div className={style.cardHeadr}>
+        <Slider images={images} />
+        <h2 className={style.title}>{title}</h2>
+      </div>
+      <div className={style.cardBody}>
+        <a href={code} target="_blank" rel="noopener noreferrer">
+          نمایش کد
+        </a>
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          مشاهده پروژه
+        </a>
+        <p className={style.description}>
+          <ExpandedText children={description} />
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/ExpandedText.tsx
+++ src/components/ExpandedText.tsx
@@ -8,20 +8,20 @@ const ExpandedText = ({ children }: Props) => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
   const spanStyle: CSSProperties = {
-    color: isExpanded ? "#c33838" : "#45ab24",
+    color: isExpanded ? "var(--red)" : " var(--green)",
     cursor: "pointer",
   };
 
   return (
     <>
-      {isExpanded ? children : children.substring(0, 100)}
+      {isExpanded ? children : children.substring(0, 120)}
       <span
         onClick={() => setIsExpanded(!isExpanded)}
         style={spanStyle}
         aria-expanded={isExpanded}
         aria-controls="expanded-text"
       >
-        {isExpanded ? "بستن" : "ادامه..."}
+        {isExpanded ? "بستن" : "... ادامه"}
       </span>
     </>
   );
