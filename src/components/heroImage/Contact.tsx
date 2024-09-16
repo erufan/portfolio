bc27dd05d0cdd9e8d9ba5e6693e978a220be5ffb
erfan taheri <erfanta197@gmail.com>
2024-09-17 02:57:30 +0330

    implement the functionality of icons

--- src/components/heroImage/Contact.tsx
+++ src/components/heroImage/Contact.tsx
@@ -3,16 +3,34 @@ import style from "./Contact.module.css";
 
 const Contact = () => {
   const icons = [
-    { Icon: FaLinkedin, key: "linkedin" },
-    { Icon: FaMailBulk, key: "mail" },
-    { Icon: FaTelegram, key: "telegram" },
-    { Icon: FaGithub, key: "github" },
+    {
+      Icon: FaLinkedin,
+      key: "linkedin",
+      link: "https://www.linkedin.com/in/erfan-taheri/",
+    },
+    {
+      Icon: FaMailBulk,
+      key: "mail",
+      link: "mailto:erfanta197@gmail.com",
+    },
+    {
+      Icon: FaTelegram,
+      key: "telegram",
+      link: "https://t.me/letnamebeNull",
+    },
+    {
+      Icon: FaGithub,
+      key: "github",
+      link: "https://github.com/erufan",
+    },
   ];
 
   return (
     <div className={style.container}>
-      {icons.map(({ Icon, key }) => (
-        <Icon key={key} className={style.icon} />
+      {icons.map(({ Icon, link, key }) => (
+        <a href={link} rel="noopener noreferrer" key={key}>
+          <Icon className={style.icon} />
+        </a>
       ))}
     </div>
   );
