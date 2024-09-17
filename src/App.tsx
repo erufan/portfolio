f29d4892a5d1d7cf01f2c3be8aa0769340df5959
erfan taheri <erfanta197@gmail.com>
2024-09-17 20:24:33 +0330

    add primary slide for page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import "./index.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-creative";
 import Page1 from "./slidePage/Page1";
+import Page2 from "./slidePage/Page2";
 
-const App = () => {
-  return <Page1 />;
-};
-
-export default App;
+export default function App() {
+  return (
+    <>
+      <Swiper grabCursor={true} spaceBetween={100} autoHeight={true}>
+        <SwiperSlide>
+          <Page1 />
+        </SwiperSlide>
+        <SwiperSlide lazy={true}>
+          <Page2 />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
