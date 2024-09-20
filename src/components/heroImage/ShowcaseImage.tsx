db0401635873b7232cf1b6375b09bb3c168b8aef
erfan taheri <erfanta197@gmail.com>
2024-09-21 00:33:49 +0330

    refactoring showcase image

--- src/components/heroImage/ShowcaseImage.tsx
+++ src/components/heroImage/ShowcaseImage.tsx
@@ -1,10 +1,22 @@
 import style from "./ShowcaseImage.module.css";
 
-const ShowcaseImage = ({ image }: { image: string }) => {
+interface Prop {
+  image: string;
+  graphicElement: string;
+}
+
+const ShowcaseImage = ({ image, graphicElement }: Prop) => {
   return (
-    <div className={style.container}>
-      <img src={image} alt="picture of me" />
-    </div>
+    <>
+      <div className={style.container}>
+        <img src={image} alt="picture of me" />
+      </div>
+      <img
+        src={graphicElement}
+        alt="graphic Element"
+        className={style.graphicElement}
+      />
+    </>
   );
 };
 
