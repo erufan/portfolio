c4a8ab92c6e3e18c1b353d6eafd4debbf18c6848
erfan taheri <erfanta197@gmail.com>
2024-09-22 17:38:49 +0330

    improve navigating through page by adding previous button in page of works

--- src/slidePage/Page2.tsx
+++ src/slidePage/Page2.tsx
@@ -1,21 +1,33 @@
+import style from "./Page2.module.css";
 import Card from "../components/Card";
 import Page2Layouts from "../layouts/Page2Layouts";
 import { data } from "../data/data";
+import { useSwiper } from "swiper/react";
 
 const Page2 = () => {
+  const swiper = useSwiper();
+
   return (
-    <Page2Layouts>
-      {data.projects.map((p) => (
-        <Card
-          key={p.title}
-          title={p.title}
-          images={p.picturs}
-          description={p.description}
-          code={p.code}
-          link={p.link}
-        />
-      ))}
-    </Page2Layouts>
+    <>
+      <span
+        className={style.navigateToPrePage}
+        onClick={() => swiper.slidePrev()}
+      >
+        بازگشت
+      </span>
+      <Page2Layouts>
+        {data.projects.map((p) => (
+          <Card
+            key={p.title}
+            title={p.title}
+            images={p.picturs}
+            description={p.description}
+            code={p.code}
+            link={p.link}
+          />
+        ))}
+      </Page2Layouts>
+    </>
   );
 };
 
