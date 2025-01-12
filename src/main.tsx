db320835e9c4f54721daaf91947adcecdceafa3a
erfan taheri <erfanta197@gmail.com>
2025-01-13 01:01:00 +0330

    add i18next for multilingual support

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
+import "./i18n";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} future={{ v7_startTransition: true }} />
