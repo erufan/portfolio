5e3cc5cc3f0034626ea8c78d5b5916febfac8e9e
erfan taheri <erfanta197@gmail.com>
2025-01-16 17:07:16 +0330

    add english description for works

--- src/routes/WorksPage..tsx
+++ src/routes/WorksPage..tsx
@@ -3,8 +3,11 @@ import Card from "../components/Card";
 import WorksPageLayouts from "./layouts/WorksPageLayouts";
 import { data } from "../data/data";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const WorksPage = () => {
+  const { i18n } = useTranslation();
+
   return (
     <>
       <Link to={"/"} className={style.navigateToPrePage}>
@@ -13,10 +16,12 @@ const WorksPage = () => {
       <WorksPageLayouts>
         {data.projects.map((p) => (
           <Card
-            key={p.title}
-            title={p.title}
+            key={p.title.en}
+            title={p.title[i18n.language as keyof typeof p.title]}
             images={p.picturs}
-            description={p.description}
+            description={
+              p.description[i18n.language as keyof typeof p.description]
+            }
             code={p.code}
             link={p.link}
           />
